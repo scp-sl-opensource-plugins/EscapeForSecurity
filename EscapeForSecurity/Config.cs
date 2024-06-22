bd3456cb0fa2d637b7f56742486617a75c04c65a
security-camera <lexaryab2010@gmail.com>
2024-06-22 17:51:14 +0300

    Добавьте файлы проекта.

---
+++ EscapeForSecurity/Config.cs
@@ -0,0 +1,15 @@
+﻿using Exiled.API.Interfaces;
+using PlayerRoles;
+
+namespace EscapeForSecurity
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+
+        public RoleTypeId RoleAfterEscape { get; set; } = RoleTypeId.NtfSergeant;
+        public RoleTypeId RoleAfterCuffedEscape { get; set; } = RoleTypeId.ChaosMarauder;
+        public float TicketsAfterEscapeCount { get; set; } = 10;
+    }
+}
---
+++ EscapeForSecurity/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using PlayerRoles;
+using Respawning;
+
+namespace EscapeForSecurity
+{
+    public class Plugin : Plugin<Config>
+    {
+        public static Plugin plugin;
+        public override string Prefix => "EscapeForSecurity";
+        public override string Name => "EscapeForSecurity";
+        public override string Author => "[OPENSOURCE PLUGIN] [https://github.com/scp-sl-opensource-plugins]";
+        public override PluginPriority Priority => PluginPriority.Last;
+
+        public override void OnEnabled()
+        {
+            plugin = this;
+            Exiled.Events.Handlers.Player.Escaping += OnEscaping;
+            base.OnEnabled();
+        }
+
+        private void OnEscaping(Exiled.Events.EventArgs.Player.EscapingEventArgs ev)
+        {
+            if (ev.Player.Role.Type is RoleTypeId.FacilityGuard && ev.IsAllowed)
+            {
+                ev.EscapeScenario = EscapeScenario.CustomEscape;
+                ev.NewRole = ev.Player.IsCuffed ? Config.RoleAfterCuffedEscape : Config.RoleAfterEscape;
+                RespawnTokensManager.RemoveTokens(ev.Player.IsCuffed ? SpawnableTeamType.NineTailedFox : SpawnableTeamType.ChaosInsurgency, Config.TicketsAfterEscapeCount);
+            }
+        }
+
+        public override void OnDisabled()
+        {
+            plugin = null;
+            Exiled.Events.Handlers.Player.Escaping -= OnEscaping;
+            base.OnDisabled();
+        }
+    }
+}
